7b443be451bc9fcd46ec9f0a1a67b502a78920eb
nkgeorge <59837223+nkgeorge@users.noreply.github.com>
2020-03-23 18:52:01 -0400

    Update test2.py
    Models in 'models' list not displaying correct 'positive' or 'negative' label.

--- transformers_pipeline/test2.py
+++ transformers_pipeline/test2.py
@@ -2,22 +2,22 @@ from transformers import pipeline
 # This pipeline uses a fine tuned DistilBert model
 # Models we can use: DistilBertConfig, AlbertConfig, CamembertConfig, XLMRobertaConfig, BartConfig, RobertaConfig, BertConfig, XLNetConfig, FlaubertConfig, XLMConfig
 # Allocate a pipeline for sentiment-analysis
-nlp = pipeline('sentiment-analysis', model='openai-gpt')
+models = ['distilroberta-base',
+          'distilbert-base-cased-distilled-squad',
+          'camembert-base',
+          'albert-base-v2',
+          'xlm-roberta-base']
 
 text = ['A monumental achievement in film and the rare film that goes from "very good" to "historically great" in its final act.',
         'Its symptomatic of an awkward, unwieldy movie that has lots of material to show you and lots of surface distractions, but nothing at all to say.',
         'Having Oblak compared to Adrian tonight was the difference and shows why Oblak is probably the best keeper in the world. Insane performance and an incredible game. Doesnt help that Adrian was shockingly bad']
-
-# Perform Sentiment Analysis on various types of text
+nlp = pipeline('sentiment-analysis')
 for x in text:
-    print(nlp(x))
+    print('Default fine tuned DistilBert model:', nlp(x))
 
-# Model:
-# [{'label': 'POSITIVE', 'score': 0.9998766}]
-# [{'label': 'NEGATIVE', 'score': 0.9979703}]
-# [{'label': 'POSITIVE', 'score': 0.99807125}]
 
-# Model: bert-base-uncased
-# [{'label': 'LABEL_1', 'score': 0.6228135}]
-# [{'label': 'LABEL_1', 'score': 0.60044444}]
-# [{'label': 'LABEL_1', 'score': 0.60955757}]
+# Perform Sentiment Analysis on text in 'text' with models in 'models
+for x in models:
+    nlp = pipeline('sentiment-analysis', model=x)   # Allocate a pipeline for sentiment-analysis
+    for y in text:
+        print(x, ":", nlp(y))   #Perform Sentiment analysis and display results
