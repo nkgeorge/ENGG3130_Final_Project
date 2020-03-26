65186487d992330f491bc913abaa22bfb393ade8
nkgeorge <59837223+nkgeorge@users.noreply.github.com>
2020-03-25 21:10:00 -0400

    Update
    Uploaded games to rip comments from and attempted to get XLMRobertaForSequenceClassification to work

--- transformers_pipeline/test2.py
+++ transformers_pipeline/test2.py
@@ -1,13 +1,12 @@
 from transformers import pipeline
-# This pipeline uses a fine tuned DistilBert model
+# This pipeline uses a fine tuned DistilBert model by default
 # Models we can use: DistilBertConfig, AlbertConfig, CamembertConfig, XLMRobertaConfig, BartConfig, RobertaConfig, BertConfig, XLNetConfig, FlaubertConfig, XLMConfig
 # Allocate a pipeline for sentiment-analysis
-
-models = ['distilroberta-base',
-          'distilbert-base-cased-distilled-squad',
+models = ['xlm-roberta-large',
+          'xlm-roberta-base',
           'camembert-base',
           'albert-base-v2',
-          'xlm-roberta-base']
+          'distilbert-base-cased-distilled-squad']
 
 text = ['A monumental achievement in film and the rare film that goes from "very good" to "historically great" in its final act.',
         'Its symptomatic of an awkward, unwieldy movie that has lots of material to show you and lots of surface distractions, but nothing at all to say.',
@@ -17,6 +16,10 @@ text = ['A monumental achievement in film and the rare film that goes from "very
 nlp = pipeline('sentiment-analysis')
 for x in text:
     print('Default fine tuned DistilBert model:', nlp(x))
+
+# nlp = pipeline('sentiment-analysis', model='XLMRobertaForSequenceClassification', tokenizer='XLMRobertaTokenizer')
+# for x in text:
+#     print('XLMRoberta:', nlp(x))
 
 
 # Perform Sentiment Analysis on text in 'text' with models in 'models
